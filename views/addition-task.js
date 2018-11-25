be903dd21652b3833c878ab48dd69e01bea2effb
Inna <inna2501@gmail.com>
2018-11-25 19:16:48 +0200

    adding functions to the addition button: reading JSON and disabling other bottons

--- views/addition-task.js
+++ views/addition-task.js
@@ -1,3 +1,22 @@
 console.log('Hello!')
 
-// 
+console.log(window.screen)  // The window.screen object contains information about the user's screen
+
+function loadTaskPlus(id) {
+
+    var xhttp = new XMLHttpRequest();
+    console.log(xhttp)
+    xhttp.onreadystatechange = function () {
+        console.log(this.readyState)
+        console.log(this.status)
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('Yes!')
+
+            var Obj = JSON.parse(this.responseText);
+            console.log(Obj)
+            //document.getElementById('dynamic-title').innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "./game-math1/targil/plus", true);
+    xhttp.send();
+}
