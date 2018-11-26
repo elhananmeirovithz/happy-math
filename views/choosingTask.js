506deb72a9681f8e785ded9a82307a2c97027de8
Inna <inna2501@gmail.com>
2018-11-26 23:21:41 +0200

    interactive design, works properly on mobile only; steps are not represented correctly yet

--- views/choosingTask.js
+++ views/choosingTask.js
@@ -2,73 +2,96 @@
 
 // console.log(window.screen)  // The window.screen object contains information about the user's screen
 
-function loadTaskPlus(id) {
+function taskPlus(id) {
 
+    // buttons of task choice disappear: 
+    document.getElementById("plus").style.display = "none";
+    document.getElementById("minus").style.display = "none";
+    document.getElementById("multiply").style.display = "none";
+    document.getElementById("division").style.display = "none";
+
+    // The XMLHttpRequest object is defined" 
     var xhttp = new XMLHttpRequest();
-    // The XMLHttpRequest object can be used to exchange data with a web server behind the scenes
+    // the XMLHttpRequest object can be used to exchange data with a web server behind the scenes
 
-    // console.log(xhttp)
+    // when the request is ready, it reads and parse the JSON with the task
     xhttp.onreadystatechange = function () {  // is called everytime the readyStatus hanges
         console.log(this.readyState)
         console.log(this.status)
         if (this.readyState == 4 && this.status == 200) {  // when the response is ready, parse the JSON to object Obj
-            console.log('Yes!')
+            console.log('request is ready!')
 
+            // object with the task: 
             var Obj = JSON.parse(this.responseText);
 
-            // presenting the JSON in the console: 
+            // printing the object in the console: 
             console.log(Obj)
 
-            document.getElementById("task-container").style.display = "flex";
-            // document.getElementById("step-container").style.display = "flex";
-            
-            // presenting the numbers in the task-container: 
-            document.getElementById("number1").innerHTML = Obj.game_number[0];
-            document.getElementById("number2").innerHTML = Obj.game_number[1];
-            
+            setTimeout(task_appears, 300)
+
+            function task_appears() {
+                document.getElementById("task-container").style.display = "flex";
+
+                // presenting the numbers in the task-container: 
+                document.getElementById("number1").innerHTML = Obj.game_number[0];
+                document.getElementById("number2").innerHTML = Obj.game_number[1];
+            }
+                                    
             // creating varibles from the JSON data: 
             var correctAnswer = Obj.game_result; 
             
             // reading steps and presenting them on the step-container 
             // a second after the presentation of the whole task: 
-            setTimeout(timing_step_present, 1000)
+            setTimeout(step_appears, 1500)
 
-            function timing_step_present() {
+            function step_appears() {
                 var step_number = 1;
                 var digit1 = 3;
                 var digit2 = 5;
+                var sign = "+"; 
+                var correctStepAnswer = 8; 
 
                 // presenting the step: 
+                document.getElementById("step-container").style.display = "flex";
+
                 document.getElementById("digit1").innerHTML = digit1;
                 document.getElementById("digit2").innerHTML = digit2;
                 document.getElementById("sign-step").innerHTML = sign;
             }
              
             // printing the results in the console:
-            console.log("correct answer is" +  " " + correctAnswer)
+            console.log("The correct answer is" +  " " + correctAnswer)
 
+
+            setTimeout(new_game_button_appears, 3000)
+
+            function new_game_button_appears() {
+                document.getElementById("new-game").style.display = "block";
+            }
         }
     };
     xhttp.open("GET", "./game-math1/targil/plus", true);  // specifies the request
     xhttp.send();  // sends the request to the server
 
-    document.getElementById("plus").style.display = "none";
-    document.getElementById("minus").style.display = "none";
-    document.getElementById("multiply").style.display = "none";
-    document.getElementById("division").style.display = "none";
-    document.getElementById("new-game").style.display = "block";
+    
+    // document.getElementById("new-game").style.
 
-    // change color after the sec: 
-    setTimeout(digit_color_change, 1000)
 
-    function digit_color_change() {
-        document.getElementById("step-container").style.display = "flex";
+    // change the color after the 1.5 sec: 
+    //setTimeout(step_appears, 1500)
 
-        document.getElementById("digit1").style.color = "#FF00FF";
-        document.getElementById("digit2").style.color = "#FF00FF";
-        document.getElementById("sign-step").style.color = "#FF00FF";
-        document.getElementById("sign-equal").style.color = "#FF00FF";
-    }
+   // function step_appears() {
+        // the task and the first steps appear when a user chooses a game type
+        //document.getElementById("task-container").style.display = "flex";
+        //document.getElementById("step-container").style.display = "flex";
+
+        //document.getElementById("digit1").style.color = "#FF00FF";
+        //document.getElementById("digit2").style.color = "#FF00FF";
+        //document.getElementById("sign-step").style.color = "#FF00FF";
+        //document.getElementById("sign-equal").style.color = "#FF00FF";
+    //}
+
+    // new-game button appears
 
 }
 
