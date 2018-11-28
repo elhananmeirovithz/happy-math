622a1fd650e2e8e83e23d0993cfa6fba651aff00
Inna <inna2501@gmail.com>
2018-11-28 22:20:34 +0200

    javascript changes

--- views/choosingTask.js
+++ views/choosingTask.js
@@ -1,3 +1,41 @@
+function evaluateInput() {
+    userInput = document.getElementById("user-answer").value;
+    console.log("user input is " + userInput)
+}
+
+
+// The task container appears and the numbers in the container appear as well according to JSON: 
+function task_appears(Obj) {
+    document.getElementById("task-container").style.display = "flex";  // task container appears
+
+    // presenting the numbers in the task-container: 
+    document.getElementById("number1").innerHTML = Obj.game_number[0];
+    document.getElementById("number2").innerHTML = Obj.game_number[1];
+}
+
+
+
+function step_appears(Obj) {
+    var sign = Obj.steps[0].sign;
+    var digit1 = Obj.steps[0].digit1;
+    var digit2 = Obj.steps[0].digit2;
+    var correctStepAnswer = Obj.steps[0].results;
+
+    // presenting the step: 
+    document.getElementById("step-container").style.display = "flex";
+
+    document.getElementById("digit1").innerHTML = digit1;
+    document.getElementById("digit2").innerHTML = digit2;
+    document.getElementById("sign-step").innerHTML = sign;
+}
+
+
+// The button 'order a new game' appears: 
+function new_game_button_appears() {
+    document.getElementById("new-game").style.display = "block";
+}
+
+
 function taskPlus(id) {
 
     // buttons of task choice disappear: 
@@ -11,53 +49,135 @@ function taskPlus(id) {
     // the XMLHttpRequest object can be used to exchange data with a web server behind the scenes
 
     // when the request is ready, it reads and parse the JSON with the task
-    xhttp.onreadystatechange = function () {  // is called everytime the readyStatus hanges
+    xhttp.onreadystatechange = function () {  // is called everytime the readyStatus changes
         //console.log(this.readyState)
         //console.log(this.status)
+
         if (this.readyState == 4 && this.status == 200) {  // when the response is ready, parse the JSON to object Obj
-            console.log('the request is ready!')
+            var Obj = JSON.parse(this.responseText);   // object with the task
 
-            // object with the task: 
-            var Obj = JSON.parse(this.responseText);
+            // the task appears 300 millisec after pressin the task name
+            setTimeout(task_appears, 300, Obj)
 
-            // printing the object in the console: 
             console.log(Obj)
 
-            setTimeout(task_appears, 300)
-
-            function task_appears() {
-                document.getElementById("task-container").style.display = "flex";
-
-                // presenting the numbers in the task-container: 
-                document.getElementById("number1").innerHTML = Obj.game_number[0];
-                document.getElementById("number2").innerHTML = Obj.game_number[1];
-            
-            }  // closes if for reading the JSON
-                                    
             // creating varibles from the JSON data: 
-            var correctTaskAnswer = Obj.game_result; 
-            var num_of_steps = Obj.steps.length; 
+            var correctTaskAnswer = Obj.game_result;
+            var num_of_steps = Obj.steps.length;
             // printing the results in the console:
             console.log("The correct task answer is " + correctTaskAnswer)
             console.log("The total steps num is " + num_of_steps)
 
+            // printing the results in the console:
+            console.log("The correct answer is" + " " + correctTaskAnswer)
+
             // reading steps and presenting them on the step-container 
             // a second after the presentation of the whole task: 
-            setTimeout(step_appears, 1500)
+            setTimeout(step_appears, 1500, Obj)
 
-            function step_appears() {
-                var sign = Obj.steps[0].sign;
-                var digit1 = Obj.steps[0].digit1;
-                var digit2 = Obj.steps[0].digit2;
-                var correctStepAnswer = Obj.steps[0].results;
+
+            // loop for reading the steps
+            //for (var i = 0; i < num_of_steps; i++) {
+            //console.log("this is step num " + (i+1))
+            //console.log(Obj.steps[i])
+            //var sign = Obj.steps[i].sign;
+            //var digit1 = Obj.steps[i].digit1;
+            //var digit2 = Obj.steps[i].digit2;
+            //var results = Obj.steps[i].results;
+            //}            
+
+
+            setTimeout(new_game_button_appears, 3000)
+
+            
+        }  // closes if for reading the JSON
+    };
+
+    xhttp.open("GET", "./game-math1/targil/plus", true);  // specifies the request
+    xhttp.send();  // sends the request to the server
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function taskPlus(id) {
+
+    // buttons of task choice disappear: 
+//    document.getElementById("plus").style.display = "none";
+//    document.getElementById("minus").style.display = "none";
+//    document.getElementById("multiply").style.display = "none";
+//    document.getElementById("division").style.display = "none";
+//
+    // The XMLHttpRequest object is defined (AJAX): 
+//    var xhttp = new XMLHttpRequest();
+    // the XMLHttpRequest object can be used to exchange data with a web server behind the scenes
+
+    // when the request is ready, it reads and parse the JSON with the task
+//    xhttp.onreadystatechange = function () {  // is called everytime the readyStatus changes
+        //console.log(this.readyState)
+        //console.log(this.status)
+
+//        if (this.readyState == 4 && this.status == 200) {  // when the response is ready, parse the JSON to object Obj
+//            var Obj = JSON.parse(this.responseText);   // object with the task
+
+            // printing the object in the console: 
+//            console.log(Obj)
+
+            // the task appears 300 millisec after pressin the task name
+//            setTimeout(task_appears, 300)
+
+            // task container appears and the numbers in the container appear as well according to JSON: 
+//            function task_appears() {
+//                document.getElementById("task-container").style.display = "flex";  // task container appears
+
+                // presenting the numbers in the task-container: 
+//                document.getElementById("number1").innerHTML = Obj.game_number[0];
+//                document.getElementById("number2").innerHTML = Obj.game_number[1];
+            
+//            }  
+                                    
+            // creating varibles from the JSON data: 
+//            var correctTaskAnswer = Obj.game_result; 
+//            var num_of_steps = Obj.steps.length; 
+            // printing the results in the console:
+//            console.log("The correct task answer is " + correctTaskAnswer)
+//            console.log("The total steps num is " + num_of_steps)
+
+            // reading steps and presenting them on the step-container 
+            // a second after the presentation of the whole task: 
+//            setTimeout(step_appears, 1500)
+
+//            function step_appears() {
+//                var sign = Obj.steps[0].sign;
+//                var digit1 = Obj.steps[0].digit1;
+//                var digit2 = Obj.steps[0].digit2;
+//                var correctStepAnswer = Obj.steps[0].results;
 
                 // presenting the step: 
-                document.getElementById("step-container").style.display = "flex";
+//                document.getElementById("step-container").style.display = "flex";
 
-                document.getElementById("digit1").innerHTML = digit1;
-                document.getElementById("digit2").innerHTML = digit2;
-                document.getElementById("sign-step").innerHTML = sign;
-            }
+//                document.getElementById("digit1").innerHTML = digit1;
+//                document.getElementById("digit2").innerHTML = digit2;
+//                document.getElementById("sign-step").innerHTML = sign;
+//            }
 
 
             // loop for reading the steps
@@ -71,21 +191,20 @@ function taskPlus(id) {
             //}            
 
 
-            setTimeout(new_game_button_appears, 3000)
+//            setTimeout(new_game_button_appears, 3000)
 
-            function new_game_button_appears() {
-                document.getElementById("new-game").style.display = "block";
+//            function new_game_button_appears() {
+//                document.getElementById("new-game").style.display = "block";
             
           // printing the results in the console:
-            console.log("The correct answer is" + " " + correctTaskAnswer)  }  // reading JSON ends
-        }
-    };
+//            console.log("The correct answer is" + " " + correctTaskAnswer)  }  // reading JSON ends
+//        }  // closes if for reading the JSON
+//    };
 
-    xhttp.open("GET", "./game-math1/targil/plus", true);  // specifies the request
-    xhttp.send();  // sends the request to the server
+//    xhttp.open("GET", "./game-math1/targil/plus", true);  // specifies the request
+//    xhttp.send();  // sends the request to the server
 
-}
-
+//}
 
 
 function loadTaskMinus(id) {
